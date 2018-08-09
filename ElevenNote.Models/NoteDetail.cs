ab907c602282b200e457e99019c8bc938aba6b76
Danielle Reed <drreed10@gmail.com>
2018-08-09 13:31:12 -0400

    update

---
+++ ElevenNote.Models/NoteDetail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteDetail
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        private DateTimeOffset createdUtc;
+
+        public DateTimeOffset GetCreatedUtc()
+        {
+            return createdUtc;
+        }
+
+        public void SetCreatedUtc(DateTimeOffset value)
+        {
+            createdUtc = value;
+        }
+
+        public DateTimeOffset? ModifiedUTC { get; set; }
+        public override string ToString() => $"[{NoteId}]{Title}";
+
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
